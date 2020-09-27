9d96f40cbbfd3fd9f54b708a07ef71af6616c158
Tnsor06 <pra006mod@gmail.com>
2020-09-27 18:23:11 +0530

    Build DOne

---
+++ build/precache-manifest.c27591e1d620dc88cd349ced14e89437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2812fedd141b87a1b92a695a10f416c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8df872b7950f346f1d7",
+    "url": "/static/css/main.635a63c4.chunk.css"
+  },
+  {
+    "revision": "21a1bc12d5ba41b111e3",
+    "url": "/static/js/2.77fcd714.chunk.js"
+  },
+  {
+    "revision": "d8df872b7950f346f1d7",
+    "url": "/static/js/main.6fb10ba1.chunk.js"
+  },
+  {
+    "revision": "1461453640a2cd11dc1d",
+    "url": "/static/js/runtime-main.b52bdbc7.js"
+  }
+]);
